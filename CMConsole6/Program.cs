024fa4cb0e0efe4f55af97c15dcf3b64e8fce03c
Þorsteinn Lemke <steini.manfredsson@gmail.com>
2021-10-22 08:00:16 +0000

    reader implemented

--- CMConsole6/Program.cs
+++ CMConsole6/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
+using Solnet.Programs;
+using Solnet.Programs.Utilities;
 using Solnet.Rpc;
 using Solnet.Rpc.Core.Http;
 using Solnet.Rpc.Models;
 using Solnet.Wallet;
+using Solnet.Wallet.Utilities;
 
 namespace CandyMachineSniper
 {
@@ -47,16 +52,91 @@ namespace CandyMachineSniper
             var isUpload = IsTransactionUpload(transaction);
             if (isUpload)
             {
-                var number = 0;
-                var instruction = transaction.Meta.InnerInstructions.Select(o=>o.Instructions.Select(i=>i.Data)).Select(o => (number++ + "\n"+ o))
-                    .Aggregate((a, b) => a += "\n\n" + b);
-                Console.WriteLine("Upload transaction found:");
-                Console.WriteLine(transaction.Transaction.Signatures.First());
+
+                var decodedInstructions = InstructionDecoder.DecodeInstructions(transaction);
+
+                var instructionData = (string)decodedInstructions.First().Values.First(o => o.Key == "Data").Value;
+
+                var bytes = new Base58Encoder().DecodeData(instructionData);
+
+                
+                var instructionJson = TryDecodeInstructionToUtf8(bytes);
+                
+                
+                Console.WriteLine("Upload transaction found:" + transaction.Slot);
+                Console.WriteLine();
                 Console.WriteLine("Upload transaction data:");
-                Console.WriteLine(instruction);
+                foreach ((string name, string uri) element in instructionJson)
+                {
+                    Console.WriteLine($"Name: {element.name}");
+                    Console.WriteLine($"Uri: {element.uri}");
+                }
             }
 
             return null;
+        }
+
+        private static List<(string name, string uri)> TryDecodeInstructionToUtf8(
+            byte[] bytes)
+        {
+            try
+            {
+                var innerSeparator = "3F000000";
+                var betweenSeparator = "19000000";
+                var firstSeparator = "000000";
+
+
+
+                var originalHex = Convert.ToHexString(bytes);
+
+
+                betweenSeparator = originalHex.Substring(32, 8);
+
+                string hex = "";
+                try
+                {
+                    hex = originalHex.Substring(originalHex.IndexOf(betweenSeparator) + betweenSeparator.Length);
+                } catch (Exception e)
+                {
+                    
+                }
+
+                var innerStrings = hex;
+                var innerElements = innerStrings.Split(betweenSeparator);
+                if (innerElements.Length > 10) innerElements = innerElements.TakeLast(10).ToArray();
+
+                try
+                {
+                    var firstSeparatorIndex = innerElements.First().IndexOf(firstSeparator);
+                    innerSeparator = innerElements.First().Substring(firstSeparatorIndex - 2, 8);
+
+                } catch (Exception e)
+                {
+                    
+                }
+
+                List<(string, string)> innerElementsSeparated = new();
+                try
+                {
+                    innerElementsSeparated = innerElements.Select(o => o.Split(firstSeparator))
+                                                              .Select(
+                                                                   o => (Encoding.UTF8.GetString(Convert.FromHexString(o[0])),
+                                                                       Encoding.UTF8.GetString(Convert.FromHexString(o[1])))
+                                                               )
+                                                              .ToList();
+                } catch (Exception e)
+                {
+                    
+                }
+
+                List<(string, string)> retList = innerElementsSeparated;
+
+                return retList;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         private static bool IsTransactionUpload(TransactionMetaSlotInfo transaction)
